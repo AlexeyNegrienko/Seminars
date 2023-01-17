d3cba26479f24e642e9b44b7dd9bc6b057956c97
Алексей <kagan5@mail.ru>
2023-01-17 11:32:56 +0300

    Seminar002

---
+++ seminar002/Program.cs
@@ -0,0 +1,54 @@
+﻿// Console.Clear();
+// int n = new Random().Next(10, 100); // [10, 99]
+// Console.WriteLine($"Случайное число: {n}");
+// int n1 = n / 10;
+// int n2 = n % 10;
+// if (n1 > n2)
+//     Console.WriteLine(n1);
+// else
+//     Console.WriteLine(n2);
+
+// //трехзначное с удалением 2 числа
+// Console.Clear();
+// int n = new Random().Next(100, 1000); // [100, 999]
+// Console.WriteLine($"Случайное число: {n}");
+// int n1 = n / 100;
+// int n3 = n % 10;
+// Console.WriteLine(n1 * 10 + n3);
+
+//Остаток
+// Console.Clear();
+// Console.Write("Введите число: ");
+// double n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+//  if (n%m==0)
+// {
+//     Console.WriteLine($"число кратное");
+// }
+// else
+// {
+//     Console.WriteLine($"{n%m}");
+// }
+
+// кратность двум числам:остаток от деления на произведение этих чисел
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+//  if (n%161==0)
+// {
+//     Console.WriteLine($"число кратное");
+// }
+//    else
+//  Console.WriteLine($"число некратное");
+
+//2 числа является ли первое квадратом второго
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (n * n == m || m * m == n)
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
