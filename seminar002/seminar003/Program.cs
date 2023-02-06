ca715f0c93f2050c1cf152b96d2d850a3425d92e
Алексей <kagan5@mail.ru>
2023-02-07 00:32:44 +0300

    Семинары

--- seminar002/seminar003/Program.cs
+++ seminar002/seminar003/Program.cs
@@ -79,3 +79,27 @@ Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i <= n; i++)
     Console.Write($"{Math.Pow(i, 3)} ");
+
+    // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D 
+Console.Clear();
+Console.Write("Введите X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double l = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1 - y2, 2)+ Math.Pow(z1 - z2, 2)), 2);
+Console.WriteLine(l);
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    Console.Write($"{Math.Pow(i, 3)} ");
