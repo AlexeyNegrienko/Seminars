011de3f33ebd2042e36949bf369807a4757ffc9c
Алексей <kagan5@mail.ru>
2023-02-07 05:38:36 +0300

    Seminar07

---
+++ seminar07/Program.cs
@@ -0,0 +1,134 @@
+﻿// // Заполнение двумерного массива
+// void InputMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// matrix[i, j] = new Random().Next(1, 100); // [1, 1000]
+// Console.Write($"{matrix[i, j]} \t");
+// }
+// Console.WriteLine();
+// }
+// }
+// Console.Clear();
+// Console.Write("Введите размеры массива: ");
+// int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+
+
+// // Задача 48: Задайте двумерный массив размера m на n,
+// //  каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// //  Выведите полученный массив на экран.
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = i + j;
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Clear();
+// Console.Write("Введите размеры массива: ");
+// int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+
+// // Задача 49
+// void InputMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+// }
+// }
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// Console.Write($"{matrix[i, j]} \t");
+// }
+// Console.WriteLine();
+// }
+// }
+// void ReleaseMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// if (i % 2 == 1 && j % 2 == 1)
+// matrix[i, j] *= matrix[i, j];
+// }
+// }
+// }
+
+// Console.Clear();
+// Console.Write("Введите размеры массива: ");
+// int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+// Console.WriteLine("Начальный массив:");
+// PrintMatrix(matrix);
+// ReleaseMatrix(matrix);
+// Console.WriteLine("Конечный массив:");
+// PrintMatrix(matrix);
+
+// // Задача 51
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+//         }
+//     }
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int ReleaseMatrix(int[,] matrix)
+// {
+//     int summa = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i == j)
+//                 summa += matrix[i, j];
+//         }
+//     }
+//     return summa;
+// }
+
+// Console.Clear();
+// Console.Write("Введите размеры массива: ");
+// int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+// Console.WriteLine("Начальный массив:");
+// PrintMatrix(matrix);
+// Console.WriteLine($"Результат: {ReleaseMatrix(matrix)}");
